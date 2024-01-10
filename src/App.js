856e823b8c5ac6523a27200c0a68b6e91c9d39d8
Piotr Obara <pobara@o2.pl>
2024-01-10 06:33:48 +0100

    change package.json

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function AccordionItem({num, title, text}) {
     setIsOpen((isOpen) => !isOpen);
   }
   return (
-    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+    <div className={`item ${isOpen ? "open" : ""}c`} onClick={handleToggle}>
       <p className='number'>{num < 9 ? `0${num +1}` : num +1}</p>
       <p className='title'>{title}</p>
       <p className='icon'>{isOpen ? "-" : "+"}</p>
